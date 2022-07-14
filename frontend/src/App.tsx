fc7005a38f5c6e305e2284dcc50671e782104a79
EdisonLemesSantos <edisonlemessantos@gmail.com>
2022-07-14 18:32:35 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./componentes/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá Mundo!</h1>
+  return (
+    // fragment<></>
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
